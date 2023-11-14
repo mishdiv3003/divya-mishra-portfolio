d21a092464b590e6a27ca791406d1159c4f01062
Divya mishra <divyamishra@Divyas-MacBook-Air.local>
2023-11-14 22:48:20 +0530

    removed unused usestate

--- src/components/AboutCard/AboutCard.jsx
+++ src/components/AboutCard/AboutCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./AboutCard.scss";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
